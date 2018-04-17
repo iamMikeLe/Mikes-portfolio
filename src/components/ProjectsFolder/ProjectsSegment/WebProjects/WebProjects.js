d651844527a198e059548963f6f8c46ab420b638
Your NameMike <iam@mike-le.info>
2018-04-17 17:26:19 +0200

    Client projects structure
    Added basic style and structure to Client Projects

--- src/components/ProjectsFolder/ProjectsSegment/WebProjects/WebProjects.js
+++ src/components/ProjectsFolder/ProjectsSegment/WebProjects/WebProjects.js
@@ -4,6 +4,7 @@ import "./WebProjetcs.css";
 
 //images
 import ViewVo from "../../../../images/vvlogo.png";
+import Vvcover from "../../../../images/vvcover.png";
 import Fibodo from "../../../../images/fibodo.png";
 import Moonrok from "../../../../images/moonrok.png";
 import Dmp from "../../../../images/dmp.png";
@@ -11,44 +12,103 @@ import Solis from "../../../../images/solis.png";
 import Pemberton from "../../../../images/pemberton.png";
 
 class WebProjects extends Component {
-    render() {
-        const fn = function () {
-            /* do your action */
-        }
+    state = {
+        selected: 0,
+        projects: [
+            {   title: "ViewVo",
+                link: "http://viewvo.com",
+                image: "link",
+                description: "ViewVo is a Web Application that provides job shadowing opportunities. Users search different kinds of jobs and activities, book time with a professional mentor and follow him for agreed duration of time to learn from him/her. In case you are a professional, who would like to pass knowledge to someone else. You can apply to be a mentor, Make some money or choose to donate it to Charity!" },
+            { 
+                title: "Fibodo",
+                link: "http://fibodo.com",
+                image: "link",
+                description: "I worked on Fibodo IOS application as a Project Manager. Fibodo is a sport oriented app for users looking for activities around them. They can search and book activities, connect and chat with organizers and much more!" },
+            { 
+                title: "MoonROK",
+                link: "http://www.moonrok.com",
+                image: "link",
+                description: "If you are searching for the latest news of K-Pop, Moonrok is your website!" },
+            { 
+                title: "DMP Dental",
+                link: "http://dmpdental.us",
+                image: "link",
+                description: "DMP Dental is a website selling high-quality dental materials. For more info, visit their website by clicking the link above." },
+            { 
+                title: "Solis Marine",
+                link: "http://solis-marine.com",
+                image: "link",
+                description: "Solis Marine is a Company website. Users can find information about their business. One of their specialties is providing independent expert advice on maritime and shipping incidents." },
+            { 
+                title: "Pemberton Capital",
+                link: "http://pembertoncapital.co.uk",
+                image: "link",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+        ]
+    }
 
+    onSelect = (selectedElement) => {
+        document.querySelector(".web-projects-icon-selector").children[this.state.selected].classList.remove('web-project-icon-selected');
+        this.setState({
+            selected: selectedElement
+        });
+        document.querySelector(".web-projects-icon-selector").children[selectedElement].classList.add('web-project-icon-selected');
+    }
+
+    componentDidMount() {
+        document.querySelector(".web-projects-icon-selector").children[this.state.selected].classList.add('web-project-icon-selected');
+    }
+
+    render() {
         return (
-            <div>
-                <div className="web-projects-icons">
-                    <img src={ViewVo} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                        <p>ViewVo</p>
+            <div className="web-projects-wrapper">
+                <div className="web-projects-icon-selector">
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(0) }}>
+                        <img src={ViewVo} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>ViewVo</p>
+                        </div>
+                    </div>
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(1) }}>
+                        <img src={Fibodo} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>Fibodo</p>
+                        </div></div>
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(2) }}>
+                        <img src={Moonrok} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>MoonROK</p>
+                        </div></div>
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(3) }}>
+                        <img src={Dmp} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>DMP</p>
+                        </div></div>
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(4) }}>
+                        <img src={Solis} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>S. Marine</p>
+                        </div></div>
+                    <div className="web-projects-icons" onClick={() => { this.onSelect(5) }}>
+                        <img src={Pemberton} alt="View Vo" height="50" width="50" />
+                        <div className="web-projects-title">
+                            <p>P. Capital</p>
+                        </div>
                     </div>
                 </div>
-                <div className="web-projects-icons">
-                    <img src={Fibodo} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                        <p>Fibodo</p>
-                    </div></div>
-                <div className="web-projects-icons">
-                    <img src={Moonrok} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                        <p>MoonROK</p>
-                    </div></div>
-                <div className="web-projects-icons">
-                    <img src={Dmp} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                        <p>DMP</p>
-                    </div></div>
-                <div className="web-projects-icons">
-                    <img src={Solis} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                        <p>S. Marine</p>
-                    </div></div>
-                <div className="web-projects-icons">
-                    <img src={Pemberton} alt="View Vo" height="50" width="50" />
-                    <div className="web-projects-title">
-                    <p>P. Capital</p>
+                <div className="web-projects-info">
+                    <div className="web-projects-info-left">
+                        <div style={{marginBottom: "10px"}}> <img src={Vvcover} alt="View Vo" /></div>
+                        <div><a href={this.state.projects[this.state.selected].link} target="_blank" rel="noopener noreferrer">{">>> Website here <<<"}</a></div>
                     </div>
+
+                    <div className="web-projects-info-right">
+                        <div>
+                            <h2>{this.state.projects[this.state.selected].title}</h2>
+                            <p> {this.state.projects[this.state.selected].description}</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         );
