985cd818c8014ea6457b825d5a797d6b59598750
Your NameMike <iam@mike-le.info>
2018-04-17 19:07:58 +0200

    firebase images
    Added firebase link images

--- src/components/ProjectsFolder/ProjectsSegment/WebProjects/WebProjects.js
+++ src/components/ProjectsFolder/ProjectsSegment/WebProjects/WebProjects.js
@@ -17,32 +17,32 @@ class WebProjects extends Component {
         projects: [
             {   title: "ViewVo",
                 link: "http://viewvo.com",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/viewVo.png?alt=media&token=834a59f6-7a5f-4c92-8159-6bbb33d48663",
                 description: "ViewVo is a Web Application that provides job shadowing opportunities. Users search different kinds of jobs and activities, book time with a professional mentor and follow him for agreed duration of time to learn from him/her. In case you are a professional, who would like to pass knowledge to someone else. You can apply to be a mentor, Make some money or choose to donate it to Charity!" },
             { 
                 title: "Fibodo",
                 link: "http://fibodo.com",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/phoneFi.png?alt=media&token=f26e6b8d-899a-44b1-a7b7-84dc6bdb437d",
                 description: "I worked on Fibodo IOS application as a Project Manager. Fibodo is a sport oriented app for users looking for activities around them. They can search and book activities, connect and chat with organizers and much more!" },
             { 
                 title: "MoonROK",
                 link: "http://www.moonrok.com",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/moonrok.png?alt=media&token=86057a6c-216d-47b8-8743-930538b7627f",
                 description: "If you are searching for the latest news of K-Pop, Moonrok is your website!" },
             { 
                 title: "DMP Dental",
                 link: "http://dmpdental.us",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/dental.png?alt=media&token=3e8d34a5-0217-4c12-9461-2babc1cdebbb",
                 description: "DMP Dental is a website selling high-quality dental materials. For more info, visit their website by clicking the link above." },
             { 
                 title: "Solis Marine",
                 link: "http://solis-marine.com",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/solis.png?alt=media&token=d07f5f7c-7a13-4a84-9d6d-24791186cf97",
                 description: "Solis Marine is a Company website. Users can find information about their business. One of their specialties is providing independent expert advice on maritime and shipping incidents." },
             { 
                 title: "Pemberton Capital",
                 link: "http://pembertoncapital.co.uk",
-                image: "link",
+                image: "https://firebasestorage.googleapis.com/v0/b/mike-s-portfolip.appspot.com/o/pemberton.png?alt=media&token=f3894cdb-18ee-4577-89fc-5b4864865aa5",
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
         ]
     }
@@ -98,7 +98,7 @@ class WebProjects extends Component {
                 </div>
                 <div className="web-projects-info">
                     <div className="web-projects-info-left">
-                        <div style={{marginBottom: "10px"}}> <img src={Vvcover} alt="View Vo" /></div>
+                        <div style={{marginBottom: "10px"}}> <img src={this.state.projects[this.state.selected].image} alt="View Vo" /></div>
                         <div><a href={this.state.projects[this.state.selected].link} target="_blank" rel="noopener noreferrer">{">>> Website here <<<"}</a></div>
                     </div>
 
