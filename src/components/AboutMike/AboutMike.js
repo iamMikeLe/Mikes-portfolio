168cbc2ccf8c3bb755c99f791459c5c06ccc415b
Your NameMike <iam@mike-le.info>
2018-04-23 19:11:42 +0200

    About me section finished

--- src/components/AboutMike/AboutMike.js
+++ src/components/AboutMike/AboutMike.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { Window, TitleBar, SegmentedControl, SegmentedControlItem, Text } from 'react-desktop/macOs';
 import Draggable, { DraggableCore } from 'react-draggable';
 import "./AboutMike.css";
+
+import ResumeRight from "./ResumeRight/ResumeRight"
+import AboutMikeRight from "./AboutMikeRight/AboutMikeRight"
 import ProfilePicture from "../../images/profilePicture.png";
-import ProjectPicture from "../../images/code.png";
-import PatiencePicture from "../../images/patience.png"
-import LanguagePicture from "../../images/language.png"
-import CatPicture from "../../images/cat.png"
 
 //componenets
 /* import Segment from "./ProjectsSegment/ProjectsSegment"; */
@@ -53,102 +52,9 @@ export default class AboutMike extends Component {
                         </div>
 
                         {/*Right side*/}
-                        <div className="AboutMike-card-about">
-                            <div className="AboutMike-card-about-inner">
-                                <div className="AboutMike-card-about-content">
-
-                                    <div className="AboutMike-card-about-title">About Me</div>
-                                    
-                                    <div className="AboutMike-row">
-                                        <div className="AboutMike-col">
-                                            <div className="AboutMike-text-box">
-                                                    <p>
-                                                        I am Hien Minh Le. For some "unknown" reason, my friends find it hard to remember, therefore, I was given a stage name “Mike” :). 
-                                                        I have rich experience in Web Development and Project Management.
-                                                    </p>
-                                            </div>
-                                            <div className="AboutMike-info-list">
-                                                <ul>
-                                                    <li><strong>Age . . . . .</strong> 25</li>
-                                                    <li><strong>Residence . . . . .</strong> Germany</li>
-                                                    <li><strong>Freelance . . . . .</strong> Available</li>
-                                                    <li><strong>Current Location . . . . .</strong> Berlin</li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div className="clear"></div>
-                                    </div>
-        
-                                </div>
-
-                                <div className="AboutMike-funFacts-row">
-                                    <div className="AboutMike-funFacts-title">Random Fact</div>
-                                    <div className="AboutMike-funFacts-content">
-
-                                        <div className="AboutMike-funFacts-content-1">
-                                            <div className="AboutMike-funFacts-content-2">
-
-                                                <div className="AboutMike-funFacts-content-3">
-                                                    <div className="AboutMike-funFacts-icon">
-                                                        <img src={ProjectPicture} width="40px" />
-                                                    </div>
-                                                </div>
-                                                <div className="AboutMike-funFacts-content-4">
-                                                    18 Projects worked on
-                                                </div>
-
-                                            </div>
-                                        </div>
-
-                                        <div className="AboutMike-funFacts-content-1">
-                                            <div className="AboutMike-funFacts-content-2">
-
-                                                <div className="AboutMike-funFacts-content-3">
-                                                    <div className="AboutMike-funFacts-icon">
-                                                    <img src={PatiencePicture} width="40px" />
-                                                    </div>
-                                                </div>
-                                                <div className="AboutMike-funFacts-content-4">
-                                                    high level of patience
-                                                </div>
-
-                                            </div>
-                                        </div>
-
-                                        <div className="AboutMike-funFacts-content-1">
-                                            <div className="AboutMike-funFacts-content-2">
-
-                                                <div className="AboutMike-funFacts-content-3">
-                                                    <div className="AboutMike-funFacts-icon">
-                                                    <img src={LanguagePicture} width="40px" />
-                                                    </div>
-                                                </div>
-                                                <div className="AboutMike-funFacts-content-4">
-                                                    2 Mother languages
-                                                </div>
-
-                                            </div>
-                                        </div>
-
-                                        <div className="AboutMike-funFacts-content-1">
-                                            <div className="AboutMike-funFacts-content-2">
-
-                                                <div className="AboutMike-funFacts-content-3">
-                                                    <div className="AboutMike-funFacts-icon">
-                                                        <img src={CatPicture} width="40px" />
-                                                    </div>
-                                                </div>
-                                                <div className="AboutMike-funFacts-content-4">
-                                                    Cat person
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
+                        <ResumeRight/>
+                        
+                        {/*<AboutMikeRight/>*/}
                     </div>
 
                 </Window>
---
+++ src/components/AboutMike/ResumeRight/ResumeRight.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import ProjectPicture from "../../../images/code.png";
+import PatiencePicture from "../../../images/patience.png"
+import LanguagePicture from "../../../images/language.png"
+import CatPicture from "../../../images/cat.png"
+
+
+class ResumeRight extends Component {
+
+    render() {
+        return (
+            <div className="AboutMike-card-about">
+                            <div className="AboutMike-card-about-inner">
+                                <div className="AboutMike-card-about-content">
+
+                                    <div className="AboutMike-card-about-title">Resume</div>
+                                    
+                                    <div className="AboutMike-row">
+                                        <div className="AboutMike-resume-col">
+                                            <div className="AboutMike-resume-title">
+                                                <div className="AboutMike-resume-icon">
+                                                    <img src={ProjectPicture} width="40px" />
+                                                </div>
+                                                <div className="AboutMike-resume-name">
+                                                    Experience
+                                                </div>
+                                            </div>
+
+                                            <div className="AboutMike-resume-item">
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Freelancer
+                                                    </div>
+
+                                                    
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Project Manager
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-company">
+                                                        MBJ London
+                                                    </div>
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Front-End Developer
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-company">
+                                                        MBJ London
+                                                    </div>
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="AboutMike-resume-col">
+                                            <div className="AboutMike-resume-title">
+                                                <div className="AboutMike-resume-icon">
+                                                    <img src={ProjectPicture} width="40px" />
+                                                </div>
+                                                <div className="AboutMike-resume-name">
+                                                    Experience
+                                                </div>
+                                            </div>
+
+                                            <div className="AboutMike-resume-item">
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Freelancer
+                                                    </div>
+
+                                                    
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Project Manager
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-company">
+                                                        MBJ London
+                                                    </div>
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+
+                                                <div className="AboutMike-resume-item-content">
+
+                                                    <div className="AboutMike-resume-item-date">
+                                                        2013 - Present
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-name">
+                                                        Front-End Developer
+                                                    </div>
+
+                                                    <div className="AboutMike-resume-item-company">
+                                                        MBJ London
+                                                    </div>
+
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="clear"></div>
+                                    </div>
+        
+                                </div>
+
+                                <div className="AboutMike-card-about-content">
+                                <div className="AboutMike-card-about-title">Skills</div>
+                                </div>
+                            </div>
+                        </div>
+        );
+    };
+}
+export default ResumeRight; 
